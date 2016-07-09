0b4de7da6788a730d9082b22076f8117172b351a
Nicole Mitchell <nicolemitchell@fb.com>
2016-07-09 16:30:40 -0700

    this is all up 2 date 4 30 PM

--- pin/MapViewController.swift
+++ pin/MapViewController.swift
@@ -15,9 +15,15 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
     var mapView = MKMapView()
     var locationManager: CLLocationManager!
     var userLocation: CLLocationCoordinate2D!
+    var pinButton = UIButton()
+    
     
     func setUpView() {
         mapView.frame = CGRect(x: 0, y: 0, width: 375, height: 667)
+        pinButton.setBackgroundImage(UIImage(named: "logo"), forState: UIControlState.Normal)
+        pinButton.frame = CGRect(x: 128, y: 482, width: 60, height: 60)
+        pinButton.addTarget(self, action: #selector(pinClicked), forControlEvents: UIControlEvents.TouchUpInside)
+        
     }
     
     override func viewDidLoad() {
@@ -26,6 +32,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
         
         setUpView()
         view.addSubview(mapView)
+        view.addSubview(pinButton)
         
         locationManager = CLLocationManager()
         locationManager.desiredAccuracy = kCLLocationAccuracyBest;
@@ -41,21 +48,23 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
             locationManager.requestWhenInUseAuthorization()
         }
         
-        
-        
         if status == .AuthorizedWhenInUse ||  status == .AuthorizedAlways {
-            userLocation = locationManager.location!.coordinate
             locationManager.startUpdatingLocation()
             locationManager.startUpdatingHeading()
+            if let userLocation = locationManager.location?.coordinate {
+                //userLocation = locationManager.location!.coordinate
+                
+                
+                //mapview setup to show user location
+                let region = MKCoordinateRegionMake(userLocation, MKCoordinateSpanMake(0.1, 0.1))
+                mapView.setRegion(region, animated: false)
+                
+                mapView.delegate = self
+                mapView.showsUserLocation = true
+                mapView.mapType = MKMapType(rawValue: 0)!
+                mapView.userTrackingMode = MKUserTrackingMode(rawValue: 2)!
+            }
             
-            //mapview setup to show user location
-            let region = MKCoordinateRegionMake(userLocation, MKCoordinateSpanMake(0.1, 0.1))
-            mapView.setRegion(region, animated: false)
-            
-            mapView.delegate = self
-            mapView.showsUserLocation = true
-            mapView.mapType = MKMapType(rawValue: 0)!
-            mapView.userTrackingMode = MKUserTrackingMode(rawValue: 2)!
         }
         
     }
@@ -75,11 +84,48 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
     
     func locationManager(manager: CLLocationManager!, didUpdateToLocation newLocation: CLLocation!, fromLocation oldLocation: CLLocation!) {
         userLocation = newLocation.coordinate
-        print("present location : \(newLocation.coordinate.latitude), \(newLocation.coordinate.longitude)")
-    
+        //print("present location : \(newLocation.coordinate.latitude), \(newLocation.coordinate.longitude)")
+        
     }
     
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        if let annotation = annotation as? Pin {
+            let identifier = "pin"
+            var view: MKPinAnnotationView
+            if let dequeuedView = mapView.dequeueReusableAnnotationViewWithIdentifier(identifier)
+                as? MKPinAnnotationView { // 2
+                dequeuedView.annotation = annotation
+                dequeuedView.draggable = true
+                view = dequeuedView
+            } else {
+                // 3
+                view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                view.draggable = true
+                view.canShowCallout = true
+                view.calloutOffset = CGPoint(x: -5, y: 5)
+                view.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure) as UIView
+            }
+            return view
+        }
+        return nil
+    }
     
+    func pinClicked() {
+        print("pin dropped")
+        var newPin = Pin.init(title: "Enter Pin Details", locationName: "", discipline: "", coordinate: userLocation)
+        
+        
+        //SEGUE TO DETAIL VIEW
+        mapView.addAnnotation(newPin as! MKAnnotation)
+    }
+    
+    func mapView(mapView: MKMapView, annotationView view: MKAnnotationView, didChangeDragState newState: MKAnnotationViewDragState, fromOldState oldState: MKAnnotationViewDragState) {
+        print("didChangeDragState")
+        if newState == MKAnnotationViewDragState.Ending{
+            let droppedAt = view.annotation?.coordinate
+            print(droppedAt)
+        }
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
